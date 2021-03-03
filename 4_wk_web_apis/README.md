16000234deb3e247dc4f887717244bcbb449a4d0
Sebastian F <sebpfave2@gmail.com>
2021-03-02 19:09:09 -0500

    Complete week 3 activity 2

---
+++ 4_wk_web_apis/README.md
@@ -0,0 +1,6 @@
+# **The George Washington University Web Developer Boot Camp**
+> Week 4 Web APIs
+
+## **Day 10 - Mar 2**
+Topics
+- Browser window object
---
+++ 4_wk_web_apis/cw_0302/02-Stu_Window-Object/script.js
@@ -0,0 +1,14 @@
+// TODO: What will the following line of code log?
+console.log(window); // will console log the web browser window object
+
+// TODO: What will the following line of code log?
+console.log(window.document); // will console log the html DOM (object model representation of the html tags). Includes DOCTYPE tag and entire html tag and its contents
+
+// TODO: What will the following line of code log?
+console.log(document.documentElement); // will console log the root element (the html tag and all its contents)
+
+// TODO: What will the following line of code log?
+console.log(document.head); // will console log the head tag element and all its content
+
+// Bonus
+console.log(window.length);
