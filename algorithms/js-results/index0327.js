4e23d5588c2c44f2d81ed089f1d7b8bd4cdece0c
Sebastian F <sebpfave2@gmail.com>
2021-03-27 09:54:39 -0400

    Complete JS code result activity

---
+++ algorithms/js-results/index0327.js
@@ -0,0 +1,20 @@
+/**
+ * What will the following code output and why:
+ * Output will be NaN
+ *
+ * Reason: declaring variables using var has them hoisted to the top of their scope
+ */
+
+var a = 1;
+function outer() {
+  var a = 2;
+  function inner() {
+    // var a; // Declaration of variable memory is hoisted up to here but value is undefined
+    a++; // tries to increment undefined
+    console.log(a);
+    var a = 5;
+  }
+  inner();
+}
+
+outer();
