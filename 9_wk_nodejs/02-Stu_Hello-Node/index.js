e00ac6691e0f9bee3a0f60cd9e77a3f7a9dc0940
Sebastian F <sebpfave2@gmail.com>
2021-04-06 19:09:47 -0400

    Complete week 9 activity 2

---
+++ 9_wk_nodejs/02-Stu_Hello-Node/index.js
@@ -0,0 +1,5 @@
+console.log("hello from node js");
+
+// Browser window object does not exist in node.js
+// console.log(window);
+// alert("Test alert");
---
+++ 9_wk_nodejs/README.md
@@ -0,0 +1,6 @@
+# **The George Washington University Web Developer Boot Camp**
+> Week 9 Node.js
+
+## **Day 25 - Apr 6**
+Topics
+- Node.js introduction
