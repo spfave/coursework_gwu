7b22029e7f0583887a09b288b8ea9ba57fbbe8a2
Sebastian F <sebpfave2@gmail.com>
2021-05-23 15:20:05 -0400

    Setup signup and login page GET routes

--- 15_wk_project2/passport/routes/homeRoutes.js
+++ 15_wk_project2/passport/routes/homeRoutes.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const { signupController, loginController } = require('../controllers');
 
+// Homepage
 router.get('/', (req, res) => {
   res.render('index');
 });
+
+// Dashboard
+// router.get('/dashboard', (req, res) => {
+//   res.render('dashboard');
+// });
+
+// Sign up page
+router.get('/signup', signupController.getSignupPage);
+
+// Login page
+router.get('/login', loginController.getLoginPage);
 
 module.exports = router;
