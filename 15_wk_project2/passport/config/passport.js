507382231ef191170d0e3c4d215c7cf14883bd6f
Sebastian F <sebpfave2@gmail.com>
2021-05-23 20:15:23 -0400

    Correct passport deserialize method

--- 15_wk_project2/passport/config/passport.js
+++ 15_wk_project2/passport/config/passport.js
@@ -28,9 +28,9 @@ passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findOne({ where: { id } });
-    done(null, user);
+    return done(null, user);
   } catch (error) {
-    done(user.errors, null);
+    return done(user.errors, null);
   }
 });
 
