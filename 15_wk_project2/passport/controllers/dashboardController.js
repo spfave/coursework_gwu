a4c0c0143f38d2d379671b53f6bccfe57b92cb56
Sebastian F <sebpfave2@gmail.com>
2021-05-24 23:30:54 -0400

    Change User model to name over username

--- 15_wk_project2/passport/controllers/dashboardController.js
+++ 15_wk_project2/passport/controllers/dashboardController.js
@@ -1,5 +1,5 @@
 const getDashboardPage = (req, res) => {
-  res.render('dashboard', { user: req.user.username });
+  res.render('dashboard', { user: req.user.name });
 };
 
 module.exports = { getDashboardPage };
