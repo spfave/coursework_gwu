a3e80c3ae69d0885fa0a6efb80fc2286617a9516
Sebastian F <sebpfave2@gmail.com>
2021-05-24 21:46:15 -0400

    Reorg and edit comments for server setup

--- 15_wk_project2/passport/server.js
+++ 15_wk_project2/passport/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
-// Server modules/packages
+// Server and middleware modules/packages
 const express = require('express');
 const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const passport = require('./config/passport');
+const connectFlash = require('connect-flash');
 
 // MVC modules/packages
 const sequelize = require('./config/connection');
@@ -15,25 +17,46 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+// Middleware - cookie parser (Not needed?)
+// app.use(cookieParser(process.env.SESS_SECRET));
+
+// Middleware - session storage
 const sess = {
   secret: process.env.SESS_SECRET,
-  cookie: { maxAge: 60 * 60 * 1000 },
+  cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 1 day
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   store: new SequelizeStore({ db: sequelize }),
 };
 app.use(session(sess));
+
+// Middleware - connect flash
+app.use(connectFlash());
+
+// Middleware - global flash variables
+app.use(function (req, res, next) {
+  res.locals.msg_success = req.flash('msg_success');
+  res.locals.msg_warn = req.flash('msg_warn');
+  res.locals.msg_error = req.flash('msg_error');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Middleware - passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-// MVC setup
+// Middleware - body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(routes);
+
+// Template Engine - Handlebars.js
 const hbs = exphbs.create();
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes);
 
 // Sync database connection and start server
 sequelize.sync({ force: false }).then(() => {
