5004cd254af57e1ca655b5185267f3a2d07ce0ef
Sebastian F <sebpfave2@gmail.com>
2021-07-06 21:30:32 -0400

    Complete week 21 activity 20

---
+++ 21_wk_mern/20-Stu_React-Router/client/src/components/ThoughtList/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+// TODO: Add code to import necessary component for creating internal hyperlinks
+import { Link } from 'react-router-dom';
+
+const ThoughtList = ({ thoughts, title }) => {
+  if (!thoughts.length) {
+    return <h3>No Thoughts Yet</h3>;
+  }
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      {thoughts &&
+        thoughts.map((thought) => (
+          <div key={thought._id} className='card mb-3'>
+            <h4 className='card-header bg-primary text-light p-2 m-0'>
+              {thought.thoughtAuthor} <br />
+              <span style={{ fontSize: '1rem' }}>
+                had this thought on {thought.createdAt}
+              </span>
+            </h4>
+            <div className='card-body bg-light p-2'>
+              <p>{thought.thoughtText}</p>
+            </div>
+            {/* TODO: Add code to create a link to this thought's single thought page at `/thoughts/<id>` */}
+            <Link
+              className='btn btn-block btn-squared btn-light text-dark'
+              to={`/thoughts/${thought._id}`}
+            >
+              View thought
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default ThoughtList;
